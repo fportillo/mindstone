83ab8d4b2a148f36eed55bd35657a09463f49a9c
Francisco Portillo <francisco.portillo@ifood.com.br>
2017-09-23 13:40:00 -0300

    Configured spark+kotlin and packaged as executable .jar file.

---
+++ src/main/kotlin/br/com/almana/MindStoneApp.kt
@@ -0,0 +1,17 @@
+package br.com.almana
+
+/**
+ * @author Francisco Portillo (francisco.portillo@ifood.com.br)
+ * Created on 9/23/17.
+ */
+import spark.kotlin.Http
+import spark.kotlin.ignite
+
+fun main(args: Array<String>) {
+    val http: Http = ignite()
+
+    http.get("/hello") {
+        "Hello Spark Kotlin!"
+    }
+}
+
